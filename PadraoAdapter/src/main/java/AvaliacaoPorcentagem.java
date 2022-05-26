99230a67659a6a53d985914f0c239947c4a7db02
AlineNeves21 <alineneves.a21@gmail.com>
2022-05-26 09:55:44 -0300

    [Add]Adding pattern adpater and diagram

---
+++ PadraoAdapter/src/main/java/AvaliacaoPorcentagem.java
@@ -0,0 +1,12 @@
+public class AvaliacaoPorcentagem {
+
+    private float porcentagem;
+
+    public float getPorcentagem() {
+        return porcentagem;
+    }
+
+    public void setPorcentagem(float porcentagem) {
+        this.porcentagem = porcentagem;
+    }
+}
